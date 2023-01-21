05c9acf2a0b7723ebc55f4c1a8326051194d96f7
roshan786143 <roshanbabu.kng@gmail.com>
2023-01-21 09:14:38 +0530

    done user authentication on login and showed him the message on login screen

--- backend/controllers/authenticatingUserLoginDetails.js
+++ backend/controllers/authenticatingUserLoginDetails.js
@@ -1,7 +1,51 @@
+const bcrypt = require('bcrypt');
+const userDetails = require('../models/userDetails');
+const jwt = require('jsonwebtoken');
+
+
 const authenticatingUserLoginDetails = (req,res)=>{
     const {email, password} = req.body;
-    // res.json(req.body)
-    res.send(email);
+    // res.send(email);
+
+    if(email.length <=0){
+        res.send('emptyField');
+        return
+    }
+
+    userDetails.findOne({
+        where : {
+            email : email
+        }
+    })
+    .then(record=>{
+        // console.log(record);
+
+        bcrypt.compare(password,record.password)
+            .then(isValidPassword=>{
+                if(isValidPassword){
+                    const secretKey = require('crypto').randomBytes(64).toString('hex');
+                    const token = jwt.sign({id : record.id,name : record.name},secretKey);
+                    console.log(token);
+                    // res.send('login successful.')
+                    res.json({password : 'validPassword',token})
+                }else{
+                    res.send('notAValidPassword')
+                }
+            })
+            .catch(err=>{
+                console.log('There\'s an error while comparing the password'.red);
+            })
+
+        // res.json(record.password);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.send('noUser')
+        // res.send('User doesn\'t exist in our db,Pls.signup.')
+    })
+    
+
+
 }
 
 module.exports = authenticatingUserLoginDetails;
--- frontend/login/login.js
+++ frontend/login/login.js
@@ -3,6 +3,7 @@ const loginData = (event) => {
 
   let email = document.getElementById('email');
   let password = document.getElementById('password');
+  
 
   email = email.value;
   password = password.value;
@@ -18,20 +19,44 @@ const loginData = (event) => {
 };
 
 const postingLoginDetails = (loginCredentials) => {
+  let displayUserLoginStatus = document.createElement('h3');
+  let form = document.getElementById('form');
   axios
     .post('http://127.0.0.1:3000/user/login', loginCredentials)
-    .then((response) => {
+    .then(response => {
       console.log('user login credentials posted successfully');
+      console.log('user token ------------------------------>');
       console.log(response);
-    })
-    .catch((err) => {
-      console.log("There's an error while posting the user login details");
-      console.log(err);
+
+      if(response.data === 'emptyField'){
+        displayUserLoginStatus.innerHTML = 'Pls.fill your login details properly';
+        form.appendChild(displayUserLoginStatus);
+        return
+      }
+      if(response.data === 'notAValidPassword'){
+        displayUserLoginStatus.innerHTML = 'User exists but password do not match';
+        form.appendChild(displayUserLoginStatus);
+        return
+      }
+      if(response.data.password === 'validPassword'){
+        displayUserLoginStatus.innerHTML = 'login successful';
+        form.appendChild(displayUserLoginStatus);
+        return
+      }
+      if(response.data === 'noUser'){
+        console.log("There's an error while posting the user login details");
+        displayUserLoginStatus.innerHTML = `User with the email - (${loginCredentials.email}) doesn\'t exist..,Pls.create an account`;
+        form.appendChild(displayUserLoginStatus)
+        return
+    }
+  })
+  .catch(err => {
+    console.log('There\'s an error while posting your login credentials to backend');
+    console.log(err);
     });
 };
 
 const goToSignUpPage = (event) => {
   event.preventDefault();
-  // console.log('ok');
   window.location.href = '../signUp/signUpPage.html';
 };
